17b19dbccf7973a598a7c6687ac6be2dda6c3597
Ricardo Aranha <ricardoaranha96@gmail.com>
2022-08-04 23:05:20 -0300

    Desafio Aula 3

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  #chave que fará a comunicação com o cloud provider
+  token = var.do_token
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s-iniciativa" {
+  name   = var.k8s_name
+  region = var.region
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.23.9-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-2vcpu-4gb"
+    node_count = 2
+  }
+}
+
+resource "digitalocean_kubernetes_node_pool" "node-premium" {
+  cluster_id = digitalocean_kubernetes_cluster.k8s-iniciativa.id
+  name       = "premium"
+  size       = "s-4vcpu-8gb"
+  node_count = 2
+}
+
+resource "local_file" "kube_config" {
+  content  = digitalocean_kubernetes_cluster.k8s-iniciativa.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
+
+variable "do_token" {}
+variable "k8s_name" {}
+variable "region" {}
+
+output "kube_endpoint" {
+  value = digitalocean_kubernetes_cluster.k8s-iniciativa.endpoint
+}
